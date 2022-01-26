8a4a65d94bd2bec129d8059a4b53c08ed8b97d36
Dario Rugerio <dario.10010@outlook.com>
2022-01-26 00:14:39 -0600

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  important: true,
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      display: ["Gilroy", "sans-serif"],
+      body: ["Graphik", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        cyan: "#9cdbff",
+      },
+      margin: {
+        96: "24rem",
+        128: "32rem",
+      },
+    },
+  },
+  variants: {
+    opacity: ["responsive", "hover"],
+  },
+  plugins: [],
+}

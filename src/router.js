3d237532064513ce9277fa9e5be96bac1bdd7cfb
Dario Rugerio <dario.10010@outlook.com>
2022-01-31 00:21:43 -0600

    dynamic routes

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import Home from "@/views/Home.vue"
 import About from "@/views/About.vue"
+import CoinDetail from "@/views/CoinDetail.vue"
 import Error from "@/views/Error.vue"
 
 const router = createRouter({
@@ -16,6 +17,11 @@ const router = createRouter({
       path: "/about",
       name: "about",
       component: About,
+    },
+    {
+      path: "/coin/:id",
+      name: "coin-detail",
+      component: CoinDetail,
     },
     {
       path: "/:pathMatch(.*)",

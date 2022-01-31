be3a708cb81ebd11e65f25dc21a4bcf3378a06ed
Dario Rugerio <dario.10010@outlook.com>
2022-01-30 20:36:34 -0600

    resolve path alias with @ https://javascript.plainenglish.io/how-to-set-up-path-resolving-in-vite-ad284e0d9eae

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Components(), vue()]
+  plugins: [Components(), vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })

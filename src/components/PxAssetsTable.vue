8e8b85449ff2030d69004bc95dc617cdecfdf388
Dario Rugerio <dario.10010@outlook.com>
2022-02-02 22:23:15 -0600

    filtering and sorting

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -3,19 +3,29 @@
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :class="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+          <span class="underline cursor-pointer" @click="changeSortOrder()"
+            >Ranking</span
+          >
         </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="item in assets"
+        v-for="item in filteredAssets"
         :key="item.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
@@ -67,10 +77,34 @@ export default {
 
   components: [PxButton],
 
+  data() {
+    return {
+      filter: "",
+      sortOrder: 1,
+    }
+  },
+
   props: {
     assets: {
       type: Array,
       default: () => [],
+    },
+  },
+
+  computed: {
+    filteredAssets() {
+      return this.assets
+        .filter(
+          a =>
+            a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            a.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder
+          }
+          return changeSortOrder
+        })
     },
   },
   methods: {
@@ -78,6 +112,9 @@ export default {
     percentF: percentFilter,
     goToCoin(id) {
       this.$router.push({ name: "coin-detail", params: { id } })
+    },
+    changeSortOrder() {
+      this.sortOrder *= -1
     },
   },
 }
